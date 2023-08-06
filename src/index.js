3362420acad34020915a73791102d242ba8b43c6
robertlisaru <lisaru.robert@agilefreaks.com>
2023-08-06 17:53:10 +0300

    Install react

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import './index.css';
 
-const elementFromHtml = (html) => {
-    const template = document.createElement("template");
-    template.innerHTML = html.trim();
-    return template.content.firstElementChild;
-};
+import { createRoot } from 'react-dom/client';
 
-const displayChild = (child) => {
-    return `
-        <li class="child">
-            ${child.name}
-        </li>
-        <li>${child.type === "directory" ? expandDirectory(child) : ""}</li>
-    `;
-};
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
 
-const expandDirectory = (directory) => {
-    return `
-        <ul class="directory">
-            ${directory.children.map(displayChild).join("")}
-        </ul>    
-    `;
-};
-
-const movieFolder = require('./movie-files.json');
-
-const appTitle = movieFolder.name;
-document.getElementById("title").textContent = appTitle;
-document.getElementById("content").appendChild(elementFromHtml(expandDirectory(movieFolder)));
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
