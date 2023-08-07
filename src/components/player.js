20a15ad4f3257399e22539e356e422b30f35853a
robertlisaru <lisaru.robert@agilefreaks.com>
2023-08-07 16:01:13 +0300

    Load subtitles

--- src/components/player.js
+++ src/components/player.js
@@ -3,6 +3,7 @@ const Player = ({ file, closePlayer }) => {
 
     const slicedFilePath = file.path.slice(file.path.indexOf("/", 3));
     const mediaPath = "/media" + slicedFilePath;
+    const subtitlePath = mediaPath.slice(0, mediaPath.lastIndexOf(".")) + ".vtt";
 
     return <div
         className="player"
@@ -13,7 +14,7 @@ const Player = ({ file, closePlayer }) => {
             width="100%"
             height="100%">
             <source src={mediaPath}></source>
-
+            <track src={subtitlePath}></track>
         </video>
     </div >;
 
