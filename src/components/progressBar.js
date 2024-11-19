5b475bb70f1fdb9be4550aad636b861224ffd255
robertlisaru <lisaru.robert@gmail.com>
2024-11-19 14:31:57 +0200

    Add color as progress bar parameter

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,12 +1,15 @@
 
-const ProgressBar = ({value}) => {
+const ProgressBar = ({ value, color = "lightgreen" }) => {
     const percentage = value * 100;
 
     return (
         <div className="progress-container">
-            <div className="progress-bar" style={{ width: `${percentage}%` }}>
+            <div className="progress-bar" style={{
+                width: `${percentage}%`,
+                backgroundColor: `${color}`
+            }}>
             </div>
-        </div>
+        </div >
     );
 };
 
