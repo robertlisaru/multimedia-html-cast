3f05347fd74263cd0f1dd68d856156a3131cbceb
robertlisaru <lisaru.robert@gmail.com>
2024-11-25 22:52:56 +0200

    Add radial gradient

--- src/components/background.js
+++ src/components/background.js
@@ -5,12 +5,14 @@ const Background = () => {
 
   useEffect(() => {
     const generateRandomGradient = () => {
-      const colors = Array.from({ length: 5 }, () =>
+      const colors = Array.from({ length: 3 }, () =>
         `rgb(${Math.random() * 64}, ${Math.random() * 64}, ${Math.random() * 64})`
       );
 
       const gradient = `linear-gradient(${Math.random() * 360}deg, ${colors.join(", ")})`;
-      return { background: gradient };
+      const radialGradient = `radial-gradient(circle at center, rgba(128, 128, 128, 0.3), rgba(0, 0, 0, 0) 70%)`;
+
+      return { background: `${radialGradient}, ${gradient}` };
     };
 
     setBackgroundStyle(generateRandomGradient());
